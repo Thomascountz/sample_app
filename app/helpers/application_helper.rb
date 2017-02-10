3d9f5b927551e39ad6674a3f938c7bf28af8044a
Thomas Countz <thomascountz@gmail.com>
2017-02-10 20:52:31 +0000

    updated title in application layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+    
+    # Returns the full page title depending on the page
+    def full_title(page_title = '')
+        base_title = "Ruby on Rails Tutorial Sample App"
+        if page_title.empty?
+            base_title
+        else
+            page_title + " | " + base_title
+        end
+    end
 end
