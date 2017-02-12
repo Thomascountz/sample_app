c5da470fe4d8385fe8c48aa5fb1742fad345a73f
Thomas Countz <thomascountz@gmail.com>
2017-02-12 01:24:51 +0000

    added initial user model validations and test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -8,4 +8,24 @@ class UserTest < ActiveSupport::TestCase
   test "should be valid" do
     assert @user.valid?
   end
+  
+  test "should be invalid without a name" do
+    @user.name = nil
+    assert_not @user.valid?
+  end
+  
+  test "should be invalid without an email" do
+    @user.email = nil
+    assert_not @user.valid?
+  end
+  
+  test "should be invalid with a name longer than 50 characters" do
+    @user.name = "a" * 51
+    assert_not @user.valid?
+  end
+  
+  test "should be invalid with an email longer than 255 characters" do
+    @user.name = "a" * 246 + "@gmail.com"
+    assert_not @user.valid?
+  end
 end
