622e4d3eb231593cc9354904ff14527bf85182cf
Thomas Countz <thomascountz@gmail.com>
2017-02-16 00:42:39 +0000

    added valid user sign up integration test

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -7,12 +7,25 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_select 'form[action="/signup"]'
     assert_no_difference 'User.count' do
       post signup_path, params: { user: { name: " ",
-                                         email: "user@invalid",
-                                         password: "foo",
-                                         password_confirmation: "bar" } }
+                                          email: "user@invalid",
+                                          password: "foo",
+                                          password_confirmation: "bar" } }
     end
     assert_template 'users/new'
     assert_select 'div#error_explaination'
     assert_select 'div.field_with_errors'
+  end
+  
+  test 'valid signup' do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post signup_path, params: { user: { name: "John Doe",
+                                          email: "johndoe@email.com",
+                                          password: "foobar",
+                                          password_confirmation: "foobar" } }
+    end
+    follow_redirect!
+    assert_template "users/show"
+    assert_not flash.empty?
   end
 end
