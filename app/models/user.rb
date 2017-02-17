f3238843a4777b8048fc583dec290172eccf157f
Thomas Countz <thomascountz@gmail.com>
2017-02-17 18:38:56 +0000

    added valid user sign in integration test

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,8 +13,8 @@ class User < ApplicationRecord
 
   # Returns the hash digest of the given string
   def User.digest(string)
-    cost = ActiveModel::SecurePassword.min_cost ? Bcrypt::Engine::MIN_COST :
-                                                  Bcrypt::Engine.cost
-    Bcrypt::Password.create(string, cost: cost)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
   end
 end
