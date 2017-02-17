6e37a66ee9d3d707c3c772cac67c16f6ddfdd4b5
Thomas Countz <thomascountz@gmail.com>
2017-02-17 17:07:51 +0000

    green lit user login integration test

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       # log in the user
     else 
-      flash[:danger] = "Invalid email or password"
+      flash.now[:danger] = "Invalid email or password"
       render 'new'
     end
   end
