f27de1616e9c8b7b9fb2c35947c7d0e680d5d0e2
Thomas Countz <thomascountz@gmail.com>
2017-02-19 02:31:59 +0000

    added logout of presistent session for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -35,6 +35,11 @@ class User < ApplicationRecord
   def authenticated?(remember_token)
     BCrypt::Password.new(self.remember_digest).is_password?(remember_token)
   end
+  
+  # Forgets a use
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
 end
 
 
