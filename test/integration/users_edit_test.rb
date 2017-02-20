e9656dfc508d5706a00833aa4adeb40dd2a95cb6
Thomas Countz <thomascountz@gmail.com>
2017-02-20 17:16:40 +0000

    added integration tests for failed user update

---
+++ test/integration/users_edit_test.rb
@@ -0,0 +1,21 @@
+require 'test_helper'
+
+class UsersEditTest < ActionDispatch::IntegrationTest
+  
+  def setup
+    @user = users(:thomas)
+  end
+  
+  test "invalid update" do
+    get edit_user_path(@user)
+    assert_template 'users/edit'
+    
+    patch user_path(@user), params: { user: { name: " ",
+                                          email: "user@invalid",
+                                          password: "foo",
+                                          password_confirmation: "bar" } }
+    assert_template 'users/edit'
+    assert_select 'div#error_explaination'
+    assert_select 'div.field_with_errors'    
+  end
+end
