fe836ca531d033a80268750359f5a66697d58fee
Thomas Countz <thomascountz@gmail.com>
2017-02-20 01:02:21 +0000

    added log_in_as test helper methods for session integration and controller tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -15,4 +15,16 @@ class ActiveSupport::TestCase
   def is_logged_in?
     !session[:user_id].nil?
   end
+  
+  # Log in as a particular user (controller tests)
+  def log_in_as(user)
+    session[:user_id] = user.id
+  end
+  
+  # Log in as a particular user (integration tests)
+  def log_in_as(user, password: 'password', remember_me: '1')
+    post login_path, params: { session: { email: user.email,
+                                          password: 'password',
+                                          remember_me: remember_me } }
+  end
 end
