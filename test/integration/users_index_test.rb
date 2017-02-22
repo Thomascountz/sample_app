04501aa07fb6053b26f20548545ebe0fb6afe56f
Thomas Countz <thomascountz@gmail.com>
2017-02-22 00:17:33 +0000

    added admin user delete tests

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -3,12 +3,13 @@ require 'test_helper'
 class UsersIndexTest < ActionDispatch::IntegrationTest
 
   def setup
-    @user = users(:thomas)
+    @admin = users(:thomas)
+    @non_admin = users(:chris)
   end
   
-  test "index including pagination" do
+  test "index as admin including pagination" do
     
-    log_in_as(@user)
+    log_in_as(@admin)
     get users_path
     
     assert_template 'users/index'
@@ -16,7 +17,19 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     
     User.paginate(page: 1).each do |user|
       assert_select 'a[href=?]', user_path(user), text: user.name
+      unless user == @admin
+        assert_select 'a[href=?]', user_path(user), text: 'delete'
+      end
     end
     
+    assert_difference 'User.count', -1 do
+      delete user_path(@non_admin)
+    end
+  end
+  
+  test "non-admin index" do
+    log_in_as(@non_admin)
+    get users_path
+    assert_select 'a', text: 'delete', count: 0
   end
 end
