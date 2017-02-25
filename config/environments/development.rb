6610303c04237c10d7678518a07ac364ed8e1d6c
Thomas Countz <thomascountz@gmail.com>
2017-02-25 00:21:14 +0000

    updated config to allow for email previews

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,9 +27,15 @@ Rails.application.configure do
   end
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
+  
+  config.action_mailer.delivery_method = :test
+  
+  host = 'sample-app-thomascountz.c9users.io'
+  
+  config.action_mailer.default_url_options = { host: host, protocol: 'https' }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -51,4 +57,5 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  
 end
